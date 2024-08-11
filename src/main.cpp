cc54ec5b73afca2862cced9fcff75ea3c59b794a
Ertuğrul SIRMA <ertugrulsirma@gmail.com>
2024-08-11 20:42:34 +0300

    added number systems exercise

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+void number_systems();
+
 int main(){
     std::cout << "Build with cmake.\n";
+    number_systems();
     return 0;
 }
---
+++ src/number_systems.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void number_systems(){
+    int number{19}; //decimal
+    std::cout << number << std::endl;
+    number = 023; //octal
+    std::cout << number << std::endl;
+    number = 0x13; //hexadecimal
+    std::cout << number << std::endl;
+    number = 0b00010011; //binary 
+    std::cout << number << std::endl;
+    
+    int number_2{197};
+    std::cout << number_2 << std::endl;
+    number_2 = 0305; // 3*8^2 + 0*8^1 + 5*8^1;
+    std::cout << number_2 << std::endl;
+    number_2 = 0xC5;  //  12*16^1 + 5*16^0;
+    std::cout << number_2 << std::endl;
+    number_2 = 0b11000101; // 1*2^7 + 1*2^6 + 1*2^2 + 1*2^0;
+    std::cout << number_2 << std::endl;
+}
